8cc85b738aea5c4f053eb624d648b32d117038e0
kacper32332 <kacperrogowski1cpp@gmail.com>
2025-01-27 20:41:59 +0100

    dodano funkcje wyszukiwania, rozpoczęto łapanie naruszenia zasady spójności

--- src/main/java/bdbt_project/SpringApplication/MyErrorController.java
+++ src/main/java/bdbt_project/SpringApplication/MyErrorController.java
@@ -1,10 +1,31 @@
 package bdbt_project.SpringApplication;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//    private static final Logger logger = Logger.getLogger(MyErrorController.class.getName());
+//
+//    @ExceptionHandler(DataIntegrityViolationException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public String handleDataIntegrityViolation(DataIntegrityViolationException ex Model model) {
+//        logger.log(Level.SEVERE, "Błąd spójności danych: " + ex.getMessage(), ex);
+//        model.addAttribute("errorMessage",
+//                "Operacja nie może zostać wykonana, ponieważ rekord jest powiązany z innymi danymi.");
+//        return "error/naruszono_wiezy";
+//    }
+//}
+
 @Controller
 public class MyErrorController implements ErrorController {
     @RequestMapping("/error")
@@ -25,9 +46,10 @@ public class MyErrorController implements ErrorController {
                 return "errors/504";
             }
             else {
-                return "others";
+//                logger.warning("Unexpected error code: " + statusCode);
+                return "error/others";
             }
         }
-        return "others";
+        return "error/others";
     }
 }
