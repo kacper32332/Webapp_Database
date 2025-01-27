16f1019c05bd28ad2ed81a5aa016ad822af0a489
cwitkows <01187393@pw.edu.pl>
2025-01-27 21:19:24 +0100

    delete access to /adresy (USER) + errors

--- src/main/java/bdbt_project/SpringApplication/SecurityConfiguration.java
+++ src/main/java/bdbt_project/SpringApplication/SecurityConfiguration.java
@@ -36,7 +36,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/main").authenticated()
                 .antMatchers("/main_admin").access("hasRole('ADMIN')")
                 .antMatchers("/main_user").access("hasRole('USER')")
-                .antMatchers("/adresy").access("hasAnyRole('ADMIN','USER')")
+                .antMatchers("/adresy").access("hasRole('ADMIN')")
                 .and()
                 .formLogin()
                 .loginPage("/login")
